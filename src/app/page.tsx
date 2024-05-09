b7689e6bc179963f37224cea9e9625072bc5dc15
MuhammadAsadAkmal <m.asadakmal1999@gmail.com>
2024-05-09 11:22:52 +0500

    links to images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
 import { db } from "~/server/db";
+import Image from "next/image";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-  "https://utfs.io/f/93fe9634-fea2-4314-956b-7ae7f700ae5b-qa0vsq.png",
-  "https://utfs.io/f/4709ed08-e620-4c42-9d65-94af53585014-lf3twj.png",
-  "https://utfs.io/f/c494ae05-dd1a-459a-b09d-bbe37a140853-qikpqu.1.png",
-];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+
+  const images = await getMyImages();
+
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+        <Image
+          src={image.url}
+          style={{ objectFit: "contain" }}
+          width={192}
+          height={192}
+          alt={image.name}
+        />
           <div>{image.name}</div>
         </div>
       ))}
